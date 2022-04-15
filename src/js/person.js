c76a2fc5f4084d2b85cc3f3424be6bca5d2f4650
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-15 10:25:26 -0700

    add venusYearsOver calc to venusYears

--- src/js/person.js
+++ src/js/person.js
@@ -61,5 +61,6 @@ export class Person {
   venusYears()  {
     this.venusAge = Math.round(this.earthYears / 0.62);
     this.venusYearsLeft = Math.round(this.yearsLeft / 0.62);
+    this.venusYearsOver = Math.round(this.yearsOver / 0.62);
   }
 }
