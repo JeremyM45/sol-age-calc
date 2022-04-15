9fa874aa048a42c2300959af8a35e6a8909e11cb
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-15 15:55:30 -0700

    refactor output logic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
+function generateOutput(person, planet) {
+  let outputName = $(`#${planet}Output`);
+  let age = planet + "Age";
+  let yearsLeft = planet + "YearsLeft";
+  let yearsOver = planet + "YearsOver";
+  if  (person.earthYears < person.lifeExpect) {
+    let output = `${capitalizeFirstChar(planet)} <br> Age: ${person[age]} <br> Expected Years Left: ${person[yearsLeft]}`;
+    return outputName.html(output);
+  } else if (person.earthYears > person.lifeExpect) {
+    let output = `${capitalizeFirstChar(planet)} <br> Age: ${person[age]} <br> Expected Years Over: ${person[yearsOver]}`;
+    return outputName.html(output);
+  }
+}
+function capitalizeFirstChar(planet)  {
+  let capArray = planet.split('');
+  capArray[0] = capArray[0].toUpperCase();
+  let planetName = capArray.join('');
+  return planetName;
+}
 $("form#info").submit(function(e) {
   e.preventDefault();
   let age = $("#age").val();
   let ethnicity = $("#region").val();
   let income = $("#income").val();
   let user = new Person(age,  ethnicity,  income);
-  if(user.lifeExpect > user.earthYears) {
-    $("#mercuryOutput").html(`Mercury <br> Age: ${user.mercuryAge} <br> Expected Years Left: ${user.mercuryYearsLeft}`);
-    $("#venusOutput").html(`Venus <br> Age: ${user.venusAge} <br> Expected Years Left: ${user.venusYearsLeft}`);
-    $("#earthOutput").html(`Earth <br> Age: ${user.earthYears} <br> Expected Years Left: ${user.yearsLeft}`);
-    $("#marsOutput").html(`Mars <br> Age: ${user.marsAge} <br> Expected Years Left: ${user.marsYearsLeft}`);
-    $("#jupiterOutput").html(`Jupiter <br> Age: ${user.jupiterAge} <br> Expected Years Left: ${user.jupiterYearsLeft}`);
-    $("#saturnOutput").html(`Saturn <br> Age: ${user.saturnAge} <br> Expected Years Left: ${user.saturnYearsLeft}`);
-    $("#uranusOutput").html(`Uranus <br> Age: ${user.uranusAge} <br> Expected Years Left: ${user.uranusYearsLeft}`);
-    $("#neptuneOutput").html(`Neptune <br> Age: ${user.neptuneAge} <br> Expected Years Left: ${user.neptuneYearsLeft}`);
-  } else  {
-    $("#mercuryOutput").html(`Age: ${user.mercuryAge} <br> Expected Years Over: ${user.mercuryYearsOver}`);
-    $("#venusOutput").html(`Venus <br> Age: ${user.venusAge} <br> Expected Years Over: ${user.venusYearsOver}`);
-    $("#earthOutput").html(`Earth <br> Age: ${user.earthYears} <br> Expected Years Over: ${user.yearsOver}`);
-    $("#marsOutput").html(`Mars <br> Age: ${user.marsAge} <br> Expected Years Over: ${user.marsYearsOver}`);
-    $("#jupiterOutput").html(`Jupiter <br> Age: ${user.jupiterAge} <br> Expected Years Over: ${user.jupiterYearsOver}`);
-    $("#saturnOutput").html(`Saturn <br> Age: ${user.saturnAge} <br> Expected Years Over: ${user.saturnYearsOver}`);
-    $("#uranusOutput").html(`Uranus <br> Age: ${user.uranusAge} <br> Expected Years Over: ${user.uranusYearsOver}`);
-    $("#neptuneOutput").html(`Neptune <br> Age: ${user.neptuneAge} <br> Expected Years Over: ${user.neptuneYearsOver}`);
-  }
-
+  generateOutput(user, "mercury");
+  generateOutput(user, "venus");
+  generateOutput(user, "earth");
+  generateOutput(user, "mars");
+  generateOutput(user, "jupiter");
+  generateOutput(user, "saturn");
+  generateOutput(user, "uranus");
+  generateOutput(user, "neptune");
 });
 import { Person } from "./js/person";
 import $ from 'jquery';
